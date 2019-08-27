a3c3aabdc68df5d181222a512807cfe2ca46e759
FreeYourSoul <ballandFyS@gmail.com>
2019-08-27 17:11:46 +0200

    [arena] : Start test script implementation

---
+++ services/arena/test/scriptTests/Contender_SampyTestCase.cpp
@@ -0,0 +1,42 @@
+// MIT License
+//
+// Copyright (c) 2019 Quentin Balland
+// Repository : https://github.com/FreeYourSoul/FyS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+//         of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+//         to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//         copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+//         copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//         AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <catch2/catch.hpp>
+
+
+/**
+ * @brief Sampy is a script representing a sample monster and also contains standalone scripts.
+ *        It is used in order to test the implementation of ChaiScript int FyS Online.
+ */
+
+TEST_CASE("Test Sampy", "[script_test]") {
+
+    SECTION("Test Enemy selection") {
+
+    } // End section : Test Enemy selection
+
+    SECTION("") {
+
+    } // End section : 
+
+} // End TestCase : Test Sampy
