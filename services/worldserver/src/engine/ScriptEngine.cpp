03e49d5315f4f76bf4b917322b5e03248cb444e0
FyS <ballandFyS@gmail.com>
2020-05-27 22:34:56 +0200

    [world] Start npc move implementation

--- services/worldserver/src/engine/ScriptEngine.cpp
+++ services/worldserver/src/engine/ScriptEngine.cpp
@@ -90,6 +90,7 @@ ScriptEngine::registerCommon()
 void
 ScriptEngine::spawnNewEncounters(const std::chrono::system_clock::time_point& currentTime)
 {
+	// todo test spawning and movement
 	for (unsigned i = 0; i < _spawningPoints.size(); ++i) {
 		if (currentTime >= _spawningPoints.at(i).nextSpawn) {
 			this->spawnEncounter(i);
