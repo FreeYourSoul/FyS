9b960d2f9faf6c8f8c4610473d0386f1ae150f67
FyS <ballandFyS@gmail.com>
2020-11-26 21:57:06 +0100

    [nix] : fix issue on travis installation

--- default.nix
+++ default.nix
@@ -4,9 +4,9 @@ in with pkgs; rec {
 
   # External Dependencies (personal)
   fil = (callPackage (builtins.fetchurl
-    "https://raw.githubusercontent.com/FreeYourSoul/FiL/9a38ffe32a7cd709efe56bf7f05255259acb95a0/fil-recipee.nix") {
-      rev = "9a38ffe32a7cd709efe56bf7f05255259acb95a0";
-    });
+        "https://raw.githubusercontent.com/FreeYourSoul/FiL/9a38ffe32a7cd709efe56bf7f05255259acb95a0/fil-recipee.nix") {
+          rev = "9a38ffe32a7cd709efe56bf7f05255259acb95a0";
+        });
 
   fseam = (callPackage (builtins.fetchurl
     "https://raw.githubusercontent.com/FreeYourSoul/FSeam/9f0ca84beab1b4c6152238820029450d589f319d/fseam-recipee.nix") {
