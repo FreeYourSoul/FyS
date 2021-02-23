8b11d5684b7c44654f62f00b7b34a9eff58b306d
FyS <ballandFyS@gmail.com>
2021-02-23 21:30:17 +0100

    update fil

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ in with pkgs; rec {
   # External Dependencies (personal)
   fil = (callPackage (builtins.fetchurl
     "https://raw.githubusercontent.com/FreeYourSoul/FiL/9a38ffe32a7cd709efe56bf7f05255259acb95a0/fil-recipee.nix") {
-      rev = "9a38ffe32a7cd709efe56bf7f05255259acb95a0";
+      rev = "903f138ab473c6249b58e1571191c71b61e912ab";
     });
 
   fseam = (callPackage (builtins.fetchurl
